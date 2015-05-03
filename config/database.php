3827f4252e6c70792a0a2ee5b0b6db63934507ec
Pavel Dimitriev <pavel.dimitriev@paymentwall.com>
2015-05-03 18:51:59 +0300

    reverted config/database.php

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
 		'pgsql' => [
 			'driver'   => 'pgsql',
 			'host'     => env('DB_HOST', 'localhost'),
-			'database' => env('DB_DATABASE', 'smcs'),
-			'username' => env('DB_USERNAME', 'smcs'),
-			'password' => env('DB_PASSWORD', 'smcs'),
+			'database' => env('DB_DATABASE', 'forge'),
+			'username' => env('DB_USERNAME', 'forge'),
+			'password' => env('DB_PASSWORD', ''),
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
